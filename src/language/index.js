1ba016b871b3d99d368d6b7504cd3d508eeb01d0
GlacierLuo <1090490148@qq.com>
2021-12-26 13:49:49 +0800

    Complete i18n configuration.

--- src/language/index.js
+++ src/language/index.js
@@ -2,15 +2,27 @@ import { createI18n } from 'vue-i18n'
 import zh from './zh-CN.json'
 import en from './en-US.json'
 
+let locale = localStorage.getItem('locale') || 'en-US'
+
 //注册i8n实例并引入语言文件
 const i18n = createI18n({
   globalInjection: true,
   legacy: false,
-  locale: 'zh-CN',
+  locale: locale,
   messages: {
     'zh-CN': zh,
     'en-US': en
   }
 })
+
+export function $t(args) {
+  return i18n.global.t(args)
+}
+
+export function changeLanguage(arg) {
+  i18n.global.locale = arg
+  localStorage.setItem('locale', arg)
+  window.location.reload()
+}
 
 export default i18n; //将i18n暴露出去，在main.js中引入挂载
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,13 +1,78 @@
 <template>
   <div>
-    <router-view />
+    <NMenu mode="horizontal" default-value="Transfer" :options="menuOptions" />
+    <router-view/>
   </div>
 </template>
 
 <script>
-export default {
-  name: "Main"
+import {defineComponent, h} from "vue"
+import {$t, changeLanguage} from '@/language'
+import {NMenu, NIcon, NDropdown, NButton} from "naive-ui"
+import {RouterLink} from "vue-router"
+import {
+  RocketOutline as TransferIcon,
+  TelescopeOutline as ExplorerIcon
+} from "@vicons/ionicons5";
+
+function renderIcon (icon) {
+  return () => h(NIcon, null, {default: () => h(icon)})
 }
+
+const menuOptions = [
+  {
+    label: () => h(
+        RouterLink, {
+          to: {
+            name: 'Transfer',
+          }
+        }, {default: () => $t('Transfer')}
+    ),
+    key: 'Transfer',
+    icon: renderIcon(TransferIcon)
+  },
+  {
+    label: () => h(
+        RouterLink, {
+          to: {
+            name: 'Explorer',
+          }
+        }, {default: () => $t('Explorer')}
+    ),
+    key: 'Explorer',
+    icon: renderIcon(ExplorerIcon)
+  },
+  {
+    label: () => h(
+        NDropdown, {
+          trigger: 'hover',
+          options: [
+            {
+              label: '中文',
+              key: 'zh-CN'
+            },
+            {
+              label: 'English',
+              key: 'en-US'
+            }
+          ],
+          'on-select': changeLanguage
+        },
+        ()=><NButton>Language</NButton>
+    ),
+    key: 'Language'
+  }
+]
+
+export default defineComponent({
+  name: 'Main',
+  components: {NMenu},
+  setup () {
+    return {
+      menuOptions
+    }
+  }
+})
 </script>
 
 <style scoped>
